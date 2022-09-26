d5be7a784cd1d171bb85e89ea2edc55ce4ab6153
bora0k <kangbr0810@gmail.com>
2022-09-26 15:02:11 +0900

    test

--- PP_Project/src/emp/EmpDTO.java
+++ PP_Project/src/emp/EmpDTO.java
@@ -1,8 +1,12 @@
 package emp;
 
 public class EmpDTO {
-	private int empno;
+	private Integer empno;
 	private String pw;
+	private String ename;
+	private String grade;
+	private Integer mgr;
+	private String admin_authority;
 	
 	public EmpDTO() {}
 
@@ -10,6 +14,48 @@ public class EmpDTO {
 		super();
 		this.empno = empno;
 		this.pw = pw;
+	}
+
+	public EmpDTO(Integer empno, String pw, String ename, String grade, Integer mgr, String admin_authority) {
+		super();
+		this.empno = empno;
+		this.pw = pw;
+		this.ename = ename;
+		this.grade = grade;
+		this.mgr = mgr;
+		this.admin_authority = admin_authority;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public Integer getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(Integer mgr) {
+		this.mgr = mgr;
+	}
+
+	public String getAdmin_authority() {
+		return admin_authority;
+	}
+
+	public void setAdmin_authority(String admin_authority) {
+		this.admin_authority = admin_authority;
 	}
 
 	public String getPw() {
@@ -30,8 +76,9 @@ public class EmpDTO {
 
 	@Override
 	public String toString() {
-		return "EmpDAO [empno=" + empno + ", pw=" + pw + "]";
+		return "EmpDTO [empno=" + empno + ", pw=" + pw + ", ename=" + ename + ", grade=" + grade + ", mgr=" + mgr
+				+ ", admin_authority=" + admin_authority + "]";
 	}
-	
+
 	
 }
